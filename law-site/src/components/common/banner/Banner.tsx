f10d77712e98e4cd2bd3ff1133382539cb954e33
codedev22 <avineshraghavendar.a@zylensolutions.com>
2024-12-24 01:19:37 +0530

    newimplementation

--- law-site/src/components/common/banner/Banner.tsx
+++ law-site/src/components/common/banner/Banner.tsx
@@ -3,9 +3,9 @@ import banimg from "../../../assets/images/banner.png";
 import { Image } from "react-bootstrap";
 function Banner() {
   return (
-    <div>
-      <div>
-        <Image src={banimg} className="img-fluid" alt="" />
+    <div className=" w-100">
+      <div className="w-100">
+        <Image src={banimg} className="banner-img w-100" alt="" />
       </div>
     </div>
   );
